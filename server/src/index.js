efc53465987288b996996cbd6efb0fb33e259e65
Imran Maisigov <ukkima.imran@gmail.com>
2026-08-08 14:49:18 +0300

    first commit

--- server/src/index.js
+++ server/src/index.js
@@ -71,7 +71,7 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
